cb9db449963d007d150d6db17060ec6d65d039d9
devmarrima <marrimacelso@gmail.com>
2025-11-01 21:53:38 -0300

    BCrypt password

--- src/main/java/com/devsuperior/demo/DemoApplication.java
+++ src/main/java/com/devsuperior/demo/DemoApplication.java
@@ -1,13 +1,28 @@
 package com.devsuperior.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication implements CommandLineRunner {
 
+	@Autowired
+	PasswordEncoder passwordEncoder;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("ENCODER = " + passwordEncoder.encode("123456"));
+		boolean result = passwordEncoder.matches("123456","$2a$10$ee6Ji5PJsjBoTqoBRnSl3OKoELf2MMQKTGqB9Grp.RShdetayoVnK");
+		System.out.println(result);
+		
+		
 	}
 
 }
